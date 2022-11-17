c541f191998ca69a39574eb7efe851793c345dfa
robipop22 <poprobertdaniel22@gmail.com>
2022-11-17 02:59:30 +0200

    add skeleton

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+const Index = () => (
+	<div className="flex justify-start items-center pt-20 h-screen w-screen flex-col text-center">
+		<h1 className="text-7xl pb-10">Welcome To Remix</h1>
+		<h2>Let's build something! 👨‍💻</h2>
+	</div>
+);
+
+export default Index;
---
+++ cypress/support/commands.ts
@@ -0,0 +1,37 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// @ts-ignore
+Cypress.Commands.add('getByTestID', testID => {
+	return cy.get(`[data-testid="${testID}"]`);
+});
+
+Cypress.on('uncaught:exception', err => {
+	// we check if the error is
+	if (err.message.includes('Minified React error #418;') || err.message.includes('Minified React error #423;')) {
+		return false;
+	}
+});
