65e4170150fbfd1ea56ffb6dce9fe612f58bf845
robipop22 <poprobertdaniel22@gmail.com>
2022-11-17 19:24:28 +0200

    trigger a production deployment

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ const Index = () => (
 	<div className="flex justify-start items-center pt-20 h-screen w-screen flex-col text-center">
 		<h1 className="text-7xl pb-10">Welcome To Remix</h1>
 		<h2>Let's build something! 👨‍💻</h2>
-		<Link to="/blog" className="bg-blue-300 p-2 mt-4">
+		<Link to="/blog" className="bg-blue-700 p-2 mt-4">
 			Check out my cool blog
 		</Link>
 	</div>
